2709310b923ca479cda5331f20eb0695e530d4b9
rvcas <x@rvcas.dev>
2021-07-20 00:12:17 -0400

    feat: init prisma

--- api/stats.go
+++ api/stats.go
@@ -1,10 +1,40 @@
 package api
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/rvcas/roc-perf-stats/prisma/db"
 )
 
 func Stats(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Stats</h1>")
+	client := db.NewClient()
+
+	if err := client.Connect(); err != nil {
+		fmt.Fprintf(w, "{\"message\": \"db connection error\"}")
+
+		return
+	}
+
+	defer func() {
+		if err := client.Prisma.Disconnect(); err != nil {
+			panic(err)
+		}
+	}()
+
+	ctx := context.Background()
+
+	benchmarks, err := client.Benchmark.FindMany().Exec(ctx)
+
+	if err != nil {
+		fmt.Fprintf(w, "{\"message\": \"error getting benchmarks\"}")
+
+		return
+	}
+
+	result, _ := json.Marshal(benchmarks)
+
+	fmt.Fprintf(w, "%s", result)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module github.com/roc-perf-stats
+module github.com/rvcas/roc-perf-stats
 
 go 1.16
+
+require (
+	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365
+	github.com/joho/godotenv v1.3.0
+	github.com/prisma/prisma-client-go v0.10.0
+	github.com/shopspring/decimal v1.2.0
+	github.com/takuoki/gocase v1.0.0
+)
